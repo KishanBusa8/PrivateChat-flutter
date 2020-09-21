6a8e89ad2342b05460c748cbe550181e3786bb93
Kishan Busa <dev020.weboccult@gmail.com>
2020-09-21 12:24:15 +0530

    remove unused code

--- lib/ChatView/ChatScreen.dart
+++ lib/ChatView/ChatScreen.dart
@@ -4,7 +4,6 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:intl/intl.dart';
-import 'package:privatechat/ChatView/ChatMessage.dart';
 import 'package:privatechat/Login/login.dart';
 
 
@@ -395,4 +394,21 @@ class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
     }
     }
 
+}
+
+extension HexColor on Color {
+  /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
+  static Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
+  /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
+  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+      '${alpha.toRadixString(16).padLeft(2, '0')}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
 }
